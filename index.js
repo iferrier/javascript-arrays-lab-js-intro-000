42e54fd8d88d125ca4d908c3cb6c8e3cdaf777a9
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-05-09 12:47:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-var kittens = [] //define your array here
+var kittens;
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  return(kittens.concat(name));
+}
+
+function prependKitten(name){
+  return(name.concat(kittens));
+}
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+destructivelyAppendKitten('Ralph');
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+destructivelyPrependKitten('Ralph');
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+destructivelyRemoveLastKitten();
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+destructivelyRemoveFirstKitten();
+
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+appendKitten('Broom');
+
+prependKitten('Arnold');
